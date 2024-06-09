9b2bca4a801264a1c65c72adbc3fac6fea151e09
james <plebjournalfoss@gmail.com>
2024-06-08 22:34:35 -0700

    remove unused query

--- app/services/queries.rb
+++ app/services/queries.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class Queries
-  def self.total_stack(user, date)
-    transactions = user.transactions.where('transaction_date <= ?', date)
-    memo = UserTransactionsSummary::TransactionsSummarizer.new
-    transactions.reduce(memo, :add_tx)
-  end
 
   def self.moving_total_fiat_value(user, end_date)
     start_date = user.transactions.order(:transaction_date).first.transaction_date
@@ -15,11 +10,16 @@ class Queries
                      .where('extract(hour from date) = 0')
                      .order(:date).to_a
     sum = UserTransactionsMovingSum.for_user(user)
-    historical_prices.map do |price|
+    historical = historical_prices.map do |price|
       {
         time: price.date.to_i,
         value: (sum.btc_at_date(price.date) * price.price).round
       }
     end
+    latest = {
+      time: DateTime.current.to_i,
+      value: (sum.btc_at_date(DateTime.current) * CurrentPrice.where(fiat_currency: user.fiat_currency).first.price).round
+    }
+    historical << latest
   end
 end
